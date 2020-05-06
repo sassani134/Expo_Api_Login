6138b3d52fcb76b52e0018ca7c07890d79c059d4
sassani134 <sassani134@gmail.com>
2020-05-06 17:03:31 +0200

    test of a new way to drawer

---
+++ navigator/DrawerContent.js
@@ -0,0 +1,46 @@
+//DrawerContent.js
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import {
+    DrawerItem,
+    DrawerContentScrollView,
+  } from '@react-navigation/drawer';
+
+export function DrawerContent(){
+    return(
+        <View>
+                    <Drawer.Navigator>
+            <Drawer.Screen
+                name="FeedScreen"
+                component={FeedScreen}
+            />
+            <Drawer.Screen name="SettingScreen" component={SettingScreen} />
+            <Drawer.Screen name="Déconexion" component={Deco}/>
+        </Drawer.Navigator>
+        </View>
+    )
+}
+
+const AppDrawer = () => {
+    return (
+        <DrawerContentScrollView>
+            <Drawer.Section>
+                <DrawerItems
+                label ="Home"
+                onPress = {() => {}}
+                />
+
+                <DrawerItems
+                label ="setting"
+                onPress = {() => {}}
+                />
+            </Drawer.Section>
+            <Drawer.Section>
+            <DrawerItems
+                label ="deconexion"
+                onPress = {() => {}}
+                />
+            </Drawer.Section>
+        </DrawerContentScrollView>
+    );
+};
